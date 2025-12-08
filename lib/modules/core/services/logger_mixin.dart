ae7c9ecfb80f959277f6dd788287a337e753d4d1
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2025-12-08 07:33:56 +0300

    fix

--- lib/modules/core/services/logger_mixin.dart
+++ lib/modules/core/services/logger_mixin.dart
@@ -1,6 +1,6 @@
 import 'package:logging/logging.dart';
 
-/// Creates a [Logger]. The name is determined by the class it is mixed
+/// Creates a [Logger]. The name is determined by the class it is mixed into.
 mixin LoggerMixin {
   Logger get logger => Logger(runtimeType.toString());
 }
