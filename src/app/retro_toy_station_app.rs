dc0b594c191734302a1fb4d80f8ce8631d38fb39
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2026-01-18 10:42:56 +0300

    fixes after review

--- src/app/retro_toy_station_app.rs
+++ src/app/retro_toy_station_app.rs
@@ -4,7 +4,7 @@ use eframe::egui;
 pub struct RetroToyStationApp {}
 
 impl RetroToyStationApp {
-    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+    pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
         // Restore app state using cc.storage (requires the "persistence" feature).
         // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ mod app;
 
 use app::RetroToyStationApp;
 
-fn main() {
+fn main() -> Result<(), eframe::Error> {
     let native_options = eframe::NativeOptions::default();
 
     eframe::run_native(
         "Retro Toy Station",
         native_options,
         Box::new(|cc| Ok(Box::new(RetroToyStationApp::new(cc)))),
-    );
+    )
 }
