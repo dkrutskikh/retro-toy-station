e034811fc03d216af1f7196107d565bffeec5a6d
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2026-01-18 10:44:48 +0300

    feat: integrate egui framework with hello world application (#2)

---
+++ src/app/mod.rs
@@ -0,0 +1,3 @@
+mod retro_toy_station_app;
+
+pub use retro_toy_station_app::RetroToyStationApp;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+mod app;
+
+use app::RetroToyStationApp;
+
+fn main() -> Result<(), eframe::Error> {
+    let native_options = eframe::NativeOptions::default();
+
+    eframe::run_native(
+        "Retro Toy Station",
+        native_options,
+        Box::new(|cc| Ok(Box::new(RetroToyStationApp::new(cc)))),
+    )
 }
